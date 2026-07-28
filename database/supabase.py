48ef55ee6d4e81b5d26a7945a4fb386f423b1757
matthewarturpyxscreen <matthewarturpyxscreen@gmail.com>
2026-07-28 16:40:06 +0700

    Update supabase.py

--- database/supabase.py
+++ database/supabase.py
@@ -1,7 +1,10 @@
 from supabase import create_client
 import streamlit as st
 
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
 supabase = create_client(
-    st.secrets["https://eiiwfizxtoelfhsmqxgs.supabase.co"],
-    st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpaXdmaXp4dG9lbGZoc21xeGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyMjI5NzUsImV4cCI6MjEwMDc5ODk3NX0.c3ylZyuOR1pIDLm7m3tI2ttzcB1rauyn4-pLwKV1vSU"]
+    SUPABASE_URL,
+    SUPABASE_KEY
 )
