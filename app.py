2a7efa0d449b9be2803faefffc24bd258ab968b9
matthewarturpyxscreen <matthewarturpyxscreen@gmail.com>
2026-07-28 16:27:17 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Operation Tracking System",
+    page_icon="📦",
+    layout="wide"
+)
+
+st.title("📦 Operation Tracking System")
+
+st.write("Selamat datang.")
