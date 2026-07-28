0c6d0322c4ab0164ff981745ac9e6d46ad3f28c1
matthewarturpyxscreen <matthewarturpyxscreen@gmail.com>
2026-07-28 16:48:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,27 @@ import streamlit as st
 st.set_page_config(
     page_title="Operation Tracking System",
     page_icon="📦",
-    layout="wide"
+    layout="wide",
 )
 
 st.title("📦 Operation Tracking System")
 
-st.write("Selamat datang.")
+st.divider()
+
+st.subheader("Login")
+
+email = st.text_input("Email")
+
+password = st.text_input(
+    "Password",
+    type="password"
+)
+
+login = st.button(
+    "Login",
+    use_container_width=True
+)
+
+if login:
+
+    st.success("Login berhasil (sementara)")
